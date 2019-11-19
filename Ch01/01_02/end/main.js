5fd4a0d0e49036ce74cedb27dd3411406169f689
Amanda Winkler <mandywinks44@gmail.com>
2019-11-19 11:08:37 -0500

    exercise files

---
+++ Ch01/01_02/end/main.js
@@ -0,0 +1,22 @@
+	// Game
+	// 	Info section
+	// 	Deck
+	// 	Discard Pile
+	// 	Rules
+
+	// Deck
+	// 	Cards
+	// 	----
+	// 	shuffle
+	// 	stack
+
+	// Card
+	// 	val
+	// 	suit
+	// 	----
+	// 	flip
+
+	// Discard Pile
+	// 	Holders
+	// 	----
+	// 	accept or reject
